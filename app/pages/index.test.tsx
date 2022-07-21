6593a86c01c70722e4e3cfbc49cc9eb5a4523d15
Thomas Shields <thomas.shields@sovtech.com>
2022-07-21 12:54:40 +0200

    feat(BlitzApp): update index.test.tsx

--- app/pages/index.test.tsx
+++ app/pages/index.test.tsx
@@ -1,14 +1,25 @@
-import "@testing-library/jest-dom/extend-expect"
-import { cleanup, render } from "@testing-library/react"
+import { render } from "test/utils"
+
+import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 import Home from "./index"
 
-const renderContainer = () => {
-  return render(<Home />)
-}
+jest.mock("app/core/hooks/useCurrentUser")
+const mockUseCurrentUser = useCurrentUser as jest.MockedFunction<typeof useCurrentUser>
 
-afterEach(cleanup)
+test.skip("renders blitz documentation link", () => {
+  // This is an example of how to ensure a specific item is in the document
+  // But it's disabled by default (by test.skip) so the test doesn't fail
+  // when you remove the the default content from the page
 
-test("Renders Home Component", async () => {
-  const component = renderContainer()
-  expect(component).toMatchSnapshot()
+  // This is an example on how to mock api hooks when testing
+  mockUseCurrentUser.mockReturnValue({
+    id: 1,
+    name: "User",
+    email: "user@email.com",
+    role: "user",
+  })
+
+  const { getByText } = render(<Home />)
+  const linkElement = getByText(/Documentation/i)
+  expect(linkElement).toBeInTheDocument()
 })
